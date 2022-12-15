aa8cf5e83921cc4133300cc35373f28304b1a271
Mark Wainwright <wainwrightml@gmail.com>
2022-12-15 16:04:19 +0000

    Put cards in the right order

--- src/data/card.rs
+++ src/data/card.rs
@@ -18,50 +18,57 @@ use strum::{EnumCount, EnumIter, EnumProperty, EnumString, IntoStaticStr};
     IntoStaticStr,
 )]
 pub enum Card {
-    #[strum(props(image_filter = "empress", name = "The Empress"))]
-    Empress,
-    #[strum(props(image_filter = "priestess", name = "The High Priestess"))]
-    Priestess,
     #[strum(props(image_filter = "beggar", name = "The Fool"))]
     Fool,
     #[strum(props(image_filter = "sorceror", name = "The Magician"))]
     Magician,
-    #[strum(props(image_filter = "moon", name = "The Moon"))]
-    Moon,
-    #[strum(props(image_filter = "wheel", name = "Wheel of Fortune"))]
-    Wheel,
-    #[strum(props(image_filter = "aerialist", name = "The Hanged Man"))]
-    Hanged,
-    #[strum(props(image_filter = "handcart", name = "The Chariot"))]
-    Chariot,
-    #[strum(props(image_filter = "kissing", name = "The Lovers"))]
-    Lovers,
+    #[strum(props(image_filter = "priestess", name = "The High Priestess"))]
+    Priestess,
+
+    #[strum(props(image_filter = "empress", name = "The Empress"))]
+    Empress,
     #[strum(props(image_filter = "king", name = "The Emperor"))]
     Emperor,
     #[strum(props(image_filter = "pope", name = "The Hierophant"))]
     Hierophant,
-    #[strum(props(image_filter = "tower", name = "The Tower"))]
-    Tower,
-    #[strum(props(image_filter = "globe", name = "The World"))]
-    World,
-    #[strum(props(image_filter = "sunrise", name = "The Sun"))]
-    Sun,
-    #[strum(props(image_filter = "archangel", name = "Temperance"))]
-    Temperance,
-    #[strum(props(image_filter = "hermit", name = "The Hermit"))]
-    Hermit,
-    #[strum(props(image_filter = "warrior", name = "Strength"))]
-    Strength,
-    #[strum(props(image_filter = "courtroom", name = "Judgement"))]
-    Judgement,
-    #[strum(props(image_filter = "shooting star", name = "The Star"))]
-    Star,
-    #[strum(props(image_filter = "grim reaper", name = "Death"))]
-    Death,
+    #[strum(props(image_filter = "kissing", name = "The Lovers"))]
+    Lovers,
+    #[strum(props(image_filter = "handcart", name = "The Chariot"))]
+    Chariot,
     #[strum(props(image_filter = "lady justice", name = "Justice"))]
     Justice,
+    #[strum(props(image_filter = "hermit", name = "The Hermit"))]
+    Hermit,
+    #[strum(props(image_filter = "wheel", name = "Wheel of Fortune"))]
+    Wheel,
+    #[strum(props(image_filter = "warrior", name = "Strength"))]
+    Strength,
+
+    #[strum(props(image_filter = "aerialist", name = "The Hanged Man"))]
+    Hanged,
+
+    #[strum(props(image_filter = "grim reaper", name = "Death"))]
+    Death,
+    #[strum(props(image_filter = "archangel", name = "Temperance"))]
+    Temperance,
     #[strum(props(image_filter = "satan", name = "The Devil"))]
     Devil,
+
+    #[strum(props(image_filter = "tower", name = "The Tower"))]
+    Tower,
+    #[strum(props(image_filter = "shooting star", name = "The Star"))]
+    Star,
+    #[strum(props(image_filter = "moon", name = "The Moon"))]
+    Moon,
+
+    #[strum(props(image_filter = "sunrise", name = "The Sun"))]
+    Sun,
+
+    #[strum(props(image_filter = "courtroom", name = "Judgement"))]
+    Judgement,
+
+    #[strum(props(image_filter = "globe", name = "The World"))]
+    World,
 }
 
 impl Card {
