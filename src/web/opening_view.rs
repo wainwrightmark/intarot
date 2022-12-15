936fca42359b9e7363b01a8a56eea25310185b53
Mark Wainwright <wainwrightml@gmail.com>
2022-12-15 11:47:40 +0000

    renamed to intarot

--- src/web/opening_view.rs
+++ src/web/opening_view.rs
@@ -12,10 +12,10 @@ pub fn opening_view() -> Html {
         <div class="sm-4 col" style="margin: auto;">
 
         <h3 style="color: gold; text-align: center;">
-        {"The Eighth Arcana"}
+        {"intarot"}
         </h3>
         <p>
-        {"The Eighth Arcana brings tarot into the space that lies beyond the limits of the world. Our lives are lost in wandering, the deterministic patterns of our destiny masked in the grey fog of randomness. Our soothsayers can help reveal the path your feet should tread."}
+        {"intarot brings tarot into the space that lies beyond the limits of the world. Our lives are lost in wandering, the deterministic patterns of our destiny masked in the grey fog of randomness. Our soothsayers can help reveal the path your feet should tread."}
         </p>
         <p>
         {"Each of our soothsayers will offer you an interpretation of the card they draw for you, but their portentous drawings may contain the seed of further truth - only you will recognise the signs that fate has chosen for you."}
