f67cbb41d4812ce4418e6377c5c893d789874d9e
Mark Wainwright <wainwrightml@gmail.com>
2022-12-20 15:41:07 +0000

    various size optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use crate::state::prelude::*;
 use crate::web::prelude::*;
 
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 pub mod data;
 pub mod state;
 pub mod web;
