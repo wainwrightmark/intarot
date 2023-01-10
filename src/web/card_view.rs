6f1dbd87e93a8aae96a5173cd1ddd8bc67d345fd
Mark Wainwright <wainwrightml@gmail.com>
2023-01-10 10:12:25 +0000

    tidied card_view

--- src/web/card_view.rs
+++ src/web/card_view.rs
@@ -20,6 +20,7 @@ pub struct CardViewProps {
     pub sign: Option<StarSign>,
 }
 
+
 #[function_component(CardView)]
 pub fn card_view(props: &CardViewProps) -> Html {
     let toggle = Dispatch::<CardPageState>::new().apply_callback(|_| ToggleDescriptionMessage {});
@@ -35,6 +36,8 @@ pub fn card_view(props: &CardViewProps) -> Html {
             });
         })
     };
+
+    let style = get_style(props.index, props.total_cards);
 
     let mut card_classes = classes!("prophecy-card");
     let mut image_classes = classes!("prophecy-image");
@@ -56,64 +59,7 @@ pub fn card_view(props: &CardViewProps) -> Html {
         card_classes.push("card-shake");
     }
 
-    let style = if props.index + 1 == props.total_cards {
-        "transform:  translateX(15em) translateY(5em) rotateZ(-30deg); visibility: hidden; pointer-events: none;".to_string()
-    } else if props.index + 1 >= props.total_cards {
-        let angle = match props.index % 4 {
-            0 => 15 + ((props.index as isize) * -10),
-            1 => -20 + ((props.index as isize) * 10),
-            2 => 20 + ((props.index as isize) * -10),
-            _ => -15 + ((props.index as isize) * 10),
-        };
 
-        let translate_x = match props.index % 4 {
-            0 => 10,
-            1 => -10,
-            2 => 20,
-            _ => -20,
-        };
-
-        let translate_y = match props.index % 4 {
-            0 => 10,
-            1 => -20,
-            2 => -10,
-            _ => 20,
-        };
-
-        format!(
-            "transform:  translateX({translate_x}em) translateY({translate_y}em) rotateZ({angle}deg); visibility: hidden; pointer-events: none;",
-        )
-    } else if top_card {
-        let angle = 0;
-        format!(
-            "transform: rotateZ({angle}deg); transition-duration: 1s, 3s"
-        )
-    } else {
-        let angle = match props.index % 4 {
-            0 => 15 + -(props.index as isize),
-            1 => -20 + (props.index as isize),
-            2 => 20 + -(props.index as isize),
-            _ => -15 + (props.index as isize),
-        };
-
-        let translate_x = match props.index % 4 {
-            0 => 1,
-            1 => -1,
-            2 => 2,
-            _ => -2,
-        };
-
-        let translate_y = match props.index % 4 {
-            0 => 1,
-            1 => 0,
-            2 => -2,
-            _ => -1,
-        };
-
-        format!(
-            "transform: translateX({translate_x}em) translateY({translate_y}em)  rotateZ({angle}deg);s; pointer-events: none;"
-        )
-    };
 
     html! {
 
@@ -168,5 +114,69 @@ pub fn card_view(props: &CardViewProps) -> Html {
                         }
                     }
         </div>
+    }
+}
+
+
+
+fn get_style(index: usize, total_cards: usize)-> String{
+    let top_card = index + 2 == total_cards;
+    if index + 1 == total_cards {
+        "transform:  translateX(15em) translateY(5em) rotateZ(-30deg); visibility: hidden; pointer-events: none;".to_string()
+    } else if index + 1 >= total_cards {
+        let angle = match index % 4 {
+            0 => 15 + ((index as isize) * -10),
+            1 => -20 + ((index as isize) * 10),
+            2 => 20 + ((index as isize) * -10),
+            _ => -15 + ((index as isize) * 10),
+        };
+
+        let translate_x = match index % 4 {
+            0 => 10,
+            1 => -10,
+            2 => 20,
+            _ => -20,
+        };
+
+        let translate_y = match index % 4 {
+            0 => 10,
+            1 => -20,
+            2 => -10,
+            _ => 20,
+        };
+
+        format!(
+            "transform:  translateX({translate_x}em) translateY({translate_y}em) rotateZ({angle}deg); visibility: hidden; pointer-events: none;",
+        )
+    } else if top_card {
+        let angle = 0;
+        format!(
+            "transform: rotateZ({angle}deg); transition-duration: 1s, 3s"
+        )
+    } else {
+        let angle = match index % 4 {
+            0 => 15 + -(index as isize),
+            1 => -20 + (index as isize),
+            2 => 20 + -(index as isize),
+            _ => -15 + (index as isize),
+        };
+
+        let translate_x = match index % 4 {
+            0 => 1,
+            1 => -1,
+            2 => 2,
+            _ => -2,
+        };
+
+        let translate_y = match index % 4 {
+            0 => 1,
+            1 => 0,
+            2 => -2,
+            _ => -1,
+        };
+
+        format!(
+            "transform: translateX({translate_x}em) translateY({translate_y}em)  rotateZ({angle}deg);s; pointer-events: none;"
+        )
     }
 }
