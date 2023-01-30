bc89a0c6364f6c28f95c2f0996cc2bbb67660e1c
cleis <cleis@users.noreply.github.com>
2023-01-30 12:52:25 +0000

    deploy: 33d721fc5e668a99c2abb97756c792b6d20fad66

--- intarot-b90868a64212b500.js
+++ intarot-b90868a64212b500.js
@@ -1143,7 +1143,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('intarot-82583386e039df09_bg.wasm', import.meta.url);
+        input = new URL('intarot-b90868a64212b500_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
