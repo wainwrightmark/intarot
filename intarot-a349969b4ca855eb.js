c122e43835be678d54f8f4c754d04ac9ac986301
wainwrightmark <wainwrightmark@users.noreply.github.com>
2023-01-31 12:21:35 +0000

    deploy: a955a34da92dad34085cbd4f2779620af00f86a7

--- intarot-a349969b4ca855eb.js
+++ intarot-a349969b4ca855eb.js
@@ -1150,7 +1150,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('intarot-c06d93a061a425d5_bg.wasm', import.meta.url);
+        input = new URL('intarot-a349969b4ca855eb_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
