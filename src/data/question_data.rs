d9dc0d432e130ba796a225c9ead324de30d85e41
Mark Wainwright <wainwrightml@gmail.com>
2023-02-02 17:11:22 +0000

    added default for question data

--- src/data/question_data.rs
+++ src/data/question_data.rs
@@ -1,9 +1,14 @@
 use serde::{Deserialize, Serialize};
-
 use super::prelude::*;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Default, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct QuestionData {
     pub guide: Guide,
     pub spread_type: SpreadType,
+}
+
+impl Default for QuestionData{
+    fn default() -> Self {
+        Self { guide: Default::default(), spread_type: SpreadType::Three }
+    }
 }
